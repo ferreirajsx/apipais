5a5dbf7b61c98117641668bcfe2261e347e3d690
ferreirajsx <tblucas14.gmail.com>
2026-04-01 18:34:34 -0300

    foi

--- script.js
+++ script.js
@@ -13,29 +13,19 @@ async function searchPais() {
         return alert("Por favor, Insira o nome de um país");
     }
 
-    try {
-        const response = await fetch(`https://restcountries.com/v3.1/name/${pais}`);
+    const response = await fetch(`https://restcountries.com/v3.1/name/${pais}`);
+    const data = await response.json();
 
-        if (!response.ok) {
-            return alert("País não encontrado. Tente novamente.");
-        }
+    const country = data[0];
 
-        const data = await response.json();
-        const country = data[0];
+    h1.innerHTML = country.name.common;
+    flag.src = country.flags.png;
+    capital.innerHTML = "Capital: " + country.capital[0];
+    populacao.innerHTML = "População: " + country.population.toLocaleString();
 
-        h1.innerHTML = country.name.common;
-        flag.src = country.flags.png;
-        capital.innerHTML = "Capital: " + country.capital[0];
-        populacao.innerHTML = "População: " + country.population.toLocaleString();
-
-        const listaLinguas = Object.values(country.languages);
-        linguas.innerHTML = "Línguas: " + listaLinguas.join(", ");
-        navigator.vibrate(200);
-
-    } catch (error) {
-        alert("Erro ao buscar o país. Verifique sua conexão.");
-        console.error(error);
-    }
+    const listaLinguas = Object.values(country.languages);
+    linguas.innerHTML = "Línguas: " + listaLinguas.join(", ");
+    navigator.vibrate(200);
 }
 
 button.addEventListener("click", searchPais);
